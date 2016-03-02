c8bb9212bce6331cea7312dac024eaea47170a43
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2016-03-02 15:25:41 -0300

    adicionado opções de tamanho e categoria na seleção de produtos do quero vender

--- catalog/controller/information/sell.php
+++ catalog/controller/information/sell.php
@@ -44,7 +44,6 @@ class ControllerInformationSell extends Controller {
 		$data['entry_name'] = $this->language->get('entry_name');
 		$data['entry_email'] = $this->language->get('entry_email');
     $data['entry_enquiry'] = $this->language->get('entry_enquiry');
-    $data['enquiry_placeholder'] = $this->language->get('enquiry_placeholder');
 		$data['entry_images'] = $this->language->get('entry_images');
 
 		if (isset($this->error['name'])) {
